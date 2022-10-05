909b5b848521f140ecc57774400fdd4fbb416439
Daniel Schreurs <daniel.schreurs@hotmail.com>
2022-10-05 10:22:28 +0200

    :sparkles: Introducing login form

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:movies/screens/home.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import 'styles/constants.dart';
 
 void main() {
   runApp(MyApp());
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:movies/styles/constants.dart';
 
 class Home extends StatefulWidget {
@@ -21,11 +22,53 @@ class _HomePageState extends State<Home> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
         bottom: false,
-        child: Center(
-          child: Text("Helloo", style: kTitle1Style),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: kHorizontalSpacer,
+            vertical: kVerticalSpacer,
+          ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text("Movies", style: kLargeTitleStyle),
+                const Text("L’application pour découvrir des nouveaux films",
+                    style: kSubtitleStyle),
+                Container(
+                  margin: const EdgeInsets.only(
+                      top: kVerticalSpacer * 2, bottom: kVerticalSpacer / 2),
+                  padding: const EdgeInsets.symmetric(
+                      vertical: kVerticalSpacer / 2,
+                      horizontal: kHorizontalSpacer),
+                  decoration: BoxDecoration(
+                      color: kCardPopupBackgroundColor,
+                      boxShadow: kBoxShadowItem,
+                      borderRadius: kBorderRadiusItem),
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          border: InputBorder.none,
+                          contentPadding: EdgeInsets.zero,
+                          hintText: 'Votre email',
+                          isDense: true,
+                          iconColor: kMainTextColor,
+                          icon: Icon(Icons.mail),
+                        ),
+                      ),
+                      const Divider(
+                        color: kMainTextColor,
+                        height: kVerticalSpacer * 2,
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
